2f47bdd9e369cd9055f6843abab0943655c50862
Faizan Khan <82697608+Refizar08@users.noreply.github.com>
2026-08-29 20:34:32 +0530

    Add

---
+++ calendar-config.js
@@ -0,0 +1,2 @@
+window.SOT_GOOGLE_CALENDAR_ID = "aced9060157a4fc2c5dc7eedb823bb47455258074c73a5b64b796e30a23053cf@group.calendar.google.com";
+window.SOT_GOOGLE_CALENDAR_URL = "https://calendar.google.com/calendar/u/0/r?cid=" + encodeURIComponent(window.SOT_GOOGLE_CALENDAR_ID);
